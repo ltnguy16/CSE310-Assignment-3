113ba05b9222b9d0f20a7339e192087a7b6f7ee1
Loi Nguyen <ltnguy16@asu.edu>
2016-02-22 13:17:26 -0700

    Version 3.2 Change testCalculator to CalculatorTest and all methods name

--- CSE360 Assignment 3/src/cse360assign3/CalculatorTest.java
+++ CSE360 Assignment 3/src/cse360assign3/CalculatorTest.java
@@ -4,16 +4,16 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-public class testCalculator {
+public class CalculatorTest {
 
 	@Test		//Test to make sure Calculator is not null when initialized
-	public void Calculator_test_null() {
+	public void testCalculator() {
 		Calculator test = new Calculator();
 		assertNotNull(test);
 	}
 	
 	@Test		//Test to see if returning the correct total
-	public void Total_test(){
+	public void testTotal(){
 		Calculator test = new Calculator();
 		test.add(7);
 		assertEquals(7, test.getTotal());
@@ -21,7 +21,7 @@ public class testCalculator {
 	}
 	
 	@Test		//Test to see if number is being added correctly
-	public void Add_test(){
+	public void testAdd(){
 		Calculator test = new Calculator();
 		test.add(7);
 		assertEquals(7, test.getTotal());
@@ -32,7 +32,7 @@ public class testCalculator {
 	}
 	
 	@Test		//Test to see if number is being subtracted correctly 
-	public void Subtract_test(){
+	public void testSubtract(){
 		Calculator test = new Calculator();
 		test.add(40);
 		test.subtract(10);
@@ -44,7 +44,7 @@ public class testCalculator {
 	}
 	
 	@Test		//Test to see if number is being multiplied correctly
-	public void Multiply_test(){
+	public void testMultiply(){
 		Calculator test = new Calculator();
 		test.multiply(7);
 		assertEquals(0 , test.getTotal());
@@ -56,7 +56,7 @@ public class testCalculator {
 	}
 	
 	@Test		//Test to see if number is being divided correctly
-	public void Divide_test(){
+	public void testDivide(){
 		Calculator test = new Calculator();
 		test.divide(2);
 		assertEquals(0, test.getTotal());
@@ -69,7 +69,7 @@ public class testCalculator {
 	}
 	
 	@Test		//Test to see if the correct history is being returned
-	public void History_test(){
+	public void testHistory(){
 		Calculator test = new Calculator();
 		assertEquals("0", test.getHistory());
 		test.add(30);
