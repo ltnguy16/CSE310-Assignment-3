35c85582597060e8e91cd79b6cfe6a5040dd5581
Loi Nguyen <ltnguy16@asu.edu>
2016-02-22 01:58:27 -0700

    JTest unit

---
+++ CSE360 Assignment 3/src/cse360assign3/testCalculator.java
@@ -0,0 +1,39 @@
+package cse360assign3;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class testCalculator {
+
+	@Test
+	public void Calculator_test_null() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void Total_test(){
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void Add_test(){
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void Subtract_test(){
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void Multiply_test(){
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void Divide_test(){
+		fail("Not yet implemented");
+	}
+
+}
