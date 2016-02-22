1109826e1956ab01b5ef9623765a5efec1534b6a
Loi Nguyen <ltnguy16@asu.edu>
2016-02-22 12:17:00 -0700

    Version 1

--- CSE360 Assignment 3/src/cse360assign3/Calculator.java
+++ CSE360 Assignment 3/src/cse360assign3/Calculator.java
@@ -1,34 +1,89 @@
 
 package cse360assign3;
 
+/**
+ * This program will set the total to zero when Calculator is initialized.
+ * It will do basic mathematics such as addition, subtraction, division,
+ * and multiplication.It will take in any integer and do the calculation 
+ * with the total. It will update the total when ever any calculation is
+ * being made.
+ * When the total is being divided by zero, the total will be set to zero.
+ * 
+ * @author Loi Nguyen
+ * @Since 2016-02-22
+ */
+
 public class Calculator {
 
 	private int total;
 	
+	
+	/**
+	 * This method will initialize Calculator and set total to zero.
+	 */
 	public Calculator () {
 		total = 0;  // not needed - included for clarity
 	}
 	
+	/**
+	 * This method will return the current total in Calculator.
+	 * 
+	 * @return	zero (for now)
+	 */
 	public int getTotal () {
 		return 0;
 	}
 	
+	/**
+	 * This method will add the value to the total and update the
+	 * total.
+	 * 
+	 * @param value			The value being added to total 
+	 */
 	public void add (int value) {
 		
 	}
 	
+	
+	/**
+	 * This method will subtract the value from the total and 
+	 * update the total.
+	 * 
+	 * @param value			The value use to subtract from the total
+	 */
 	public void subtract (int value) {
 		
 	}
 	
+	
+	/**
+	 * This method will multiply the value to the total and update
+	 * the total.
+	 *  
+	 * @param value			The value multiply to the total
+	 */
 	public void multiply (int value) {
 		
 	}
 	
+	
+	/**
+	 * This method will divide the value from the total and 
+	 * update the total.
+	 *  
+	 * @param value			The value use to divide from the total
+	 */
 	public void divide (int value) {
 		
 	}
 	
+	
+	/**
+	 * This method will return the history of the methods and number
+	 * that has been used.
+	 *  
+	 * @return				Empty string (for now)
+	 */
 	public String getHistory () {
 		return "";
 	}
--- CSE360 Assignment 3/src/cse360assign3/testCalculator.java
+++ CSE360 Assignment 3/src/cse360assign3/testCalculator.java
@@ -6,38 +6,38 @@ import org.junit.Test;
 
 public class testCalculator {
 
-	@Test
+	@Test		//Test to make sure Calculator is not null when initialized
 	public void Calculator_test_null() {
 		Calculator test = new Calculator();
 		assertNotNull(test);
 	}
 	
-	@Test
+	@Test		//Test to see if returning the correct total
 	public void Total_test(){
 		fail("Not yet implemented");
 	}
 	
-	@Test
+	@Test		//Test to see if number is being added correctly
 	public void Add_test(){
 		fail("Not yet implemented");
 	}
 	
-	@Test
+	@Test		//Test to see if number is being subtracted correctly 
 	public void Subtract_test(){
 		fail("Not yet implemented");
 	}
 	
-	@Test
+	@Test		//Test to see if number is being multiplied correctly
 	public void Multiply_test(){
 		fail("Not yet implemented");
 	}
 	
-	@Test
+	@Test		//Test to see if number is being divided correctly
 	public void Divide_test(){
 		fail("Not yet implemented");
 	}
 	
-	@Test
+	@Test		//Test to see if the correct history is being returned
 	public void History_test(){
 		fail("Not yet implemented");
 	}
